ca98cc6499ca7effbcc6be203f6f76a1ed0bf708
apoyando <apoyando@yahoo.com>
2019-01-23 15:45:38 -0600

    hello world

--- store/index.js
+++ store/index.js
@@ -1,21 +1,21 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
-import event from './event'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import user from './user';
+import event from './event';
 
 const reducer = combineReducers({
   user,
   event
-})
+});
 
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
 
-const store = createStore(reducer, middleware)
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './user'
-export * from './event'
+export default store;
+export * from './user';
+export * from './event';
