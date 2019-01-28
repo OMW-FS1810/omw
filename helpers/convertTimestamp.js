b8677b0c8e4f59d5c7dd50da77234ae869335280
apoyando <apoyando@yahoo.com>
2019-01-27 18:54:56 -0600

    small fix

--- helpers/convertTimestamp.js
+++ helpers/convertTimestamp.js
@@ -1,5 +1,5 @@
 export const convertTimestamp = timestamp => {
-  var d = new Date(timestamp), // Convert the passed timestamp to milliseconds  * 1000
+  var d = new Date(timestamp),
     yyyy = d.getFullYear(),
     mm = ('0' + (d.getMonth() + 1)).slice(-2), // Months are zero based. Add leading 0.
     dd = ('0' + d.getDate()).slice(-2), // Add leading 0.
