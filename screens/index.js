c8a28fe57635b612a4e2aa448a177a2b817a166b
Jingtin Tim Lin <lin.jingt@gmail.com>
2019-01-28 15:14:44 -0600

    adjusted export defaults

--- screens/index.js
+++ screens/index.js
@@ -1,12 +1,9 @@
 export { default as CreateEvent } from './CreateEvent';
 export { default as EventMap } from './EventMap';
-export { default as Screen1 } from './Screen1';
-export { default as Screen2 } from './Screen2';
-export { default as Screen3 } from './Screen3';
-export { default as SignupScreen } from './SignupScreen';
-export { default as LoginScreen } from './LoginScreen';
-export { default as ForgotPasswordScreen } from './ForgotPasswordScreen';
+export { default as Signup } from './Signup';
 export { default as Auth } from './Auth';
 export { default as Invite } from './Invite';
 export { default as Profile } from './Profile';
 export { default as UserProfile } from './UserProfile';
+export { default as Settings } from './Settings';
+export {default as AuthLoading} from './AuthLoading'
