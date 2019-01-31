4e79d1338e56ec3568fc29bab97c451d52336b1a
Rami Dihu <rami.dihu@gmail.com>
2019-01-31 11:08:32 -0600

    v1

--- screens/SingleEvent.js
+++ screens/SingleEvent.js
@@ -2,47 +2,64 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Dimensions
+  Text
 } from 'react-native';
 import { setSingleEvent } from '../redux/store';
 import { connect } from 'react-redux';
-
-var { height } = Dimensions.get('window');
-
-var boxCount = 3;
-var boxHeight = height / boxCount;
-
-class SingleEvent extends Component {
-  render() {
-    console.log('email in events page', this.props)
-    return (
-        <View style={styles.container}>
-            <View style={[styles.box, styles.box1]} />
-            <View style={[styles.box, styles.box2]} />
-            <View style={[styles.box, styles.box3]} />
-        </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column'
   },
-  box: {
-    height: boxHeight
-  },
   box1: {
-    backgroundColor: '#2196F3'
+    backgroundColor: '#e3aa1a',
+    height: 75
   },
   box2: {
-    backgroundColor: '#8BC34A'
+    backgroundColor: 'grey',
+    height: 75
   },
   box3: {
-    backgroundColor: '#e3aa1a'
+    backgroundColor: 'white',
+    height: 250
+  },
+  box4: {
+    backgroundColor: 'grey',
+    height: 75
+  },
+  box5: {
+    backgroundColor: 'white',
+    height: 200
   }
 });
+
+class SingleEvent extends Component {
+  render() {
+    console.log('email in events page', this.props)
+    return (
+        <View style={styles.container}>
+            <View style={styles.box1}>
+              <Text>Event</Text>
+            </View>
+            <View style={styles.box2}>
+              <Text>Details</Text>
+            </View>
+            <View style={styles.box3}>
+              <Text>Location</Text>
+            </View>
+            <View style={styles.box4}>
+              <Text>Invitees</Text>
+            </View>
+            <View style={styles.box5}>
+              <Text>Names</Text>
+            </View>
+        </View>
+    );
+  }
+}
+
+
 
 const mapStateToProps = state => ({
   email: state.email
