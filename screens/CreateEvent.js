0a212115784ef74ee097dae72adeada1b4415833
Jingtin Tim Lin <lin.jingt@gmail.com>
2019-02-04 14:09:32 -0600

    create events

--- screens/CreateEvent.js
+++ screens/CreateEvent.js
@@ -2,11 +2,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import DatePicker from 'react-native-datepicker';
-import { Surface } from 'react-native-paper';
 import { connect } from 'react-redux';
 import { populateEventDeets, fetchAllEvents } from '../redux/event';
 import { GOOGLE_API } from '../secrets';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+import * as theme from '../styles/theme';
+const { padding, color, fontFamily, fontSize, windowWidth, normalize } = theme;
 
 class CreateEvent extends Component {
   state = {
@@ -20,9 +22,6 @@ class CreateEvent extends Component {
     },
     host: ''
   };
-
-
-  // static navigationOptions = {title: 'Create Event'}
 
   componentDidMount() {
     if (this.props.user) {
@@ -52,122 +51,124 @@ class CreateEvent extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.titleView}>
-          <Text style={styles.title}>Create an event!</Text>
+          <Text style={styles.title}>Create Event</Text>
         </View>
-        <TextInput
-          style={styles.input}
-          mode="outlined"
-          label="Event Name"
-          value={this.state.name}
-          onChangeText={name => this.setState({ name })}
-        />
 
-        <GooglePlacesAutocomplete
-          style={styles.location}
-          placeholder="Event Location"
-          minLength={2} // minimum length of text to search
-          autoFocus={false}
-          listViewDisplayed="false" // true/false/undefined
-          fetchDetails={true}
-          renderDescription={row => row.description} // custom description render
-          onPress={async (data, details = null) => {
-            // 'details' is provided when fetchDetails = true
-            await this.setState({
-              location: {
-                locationName: data.structured_formatting.main_text,
-                locationAddress: data.description,
-                locationGeocode: details.geometry.location
+          <TextInput
+            style={styles.inputContainer}
+            placeholder="Event Name"
+            placeholderTextColor="#aaa"
+            clearButtonMode="while-editing"
+            value={this.state.name}
+            onChangeText={name => this.setState({ name })}
+          />
+        <View style={styles.content}>
+
+          <GooglePlacesAutocomplete
+            style={styles.location}
+            placeholder="Event Location"
+            minLength={2} // minimum length of text to search
+            autoFocus={false}
+            listViewDisplayed="false" // true/false/undefined
+            fetchDetails={true}
+            renderDescription={row => row.description} // custom description render
+            onPress={async (data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              await this.setState({
+                location: {
+                  locationName: data.structured_formatting.main_text,
+                  locationAddress: data.description,
+                  locationGeocode: details.geometry.location
+                }
+              });
+            }}
+            getDefaultValue={() => ''}
+            query={{
+              // available options: https://developers.google.com/places/web-service/autocomplete
+              key: GOOGLE_API,
+              language: 'en' // language of the results
+              // types: '(cities)' // default: 'geocode'
+            }}
+            styles={{
+              container: {
+                flex: 1,
+                width: windowWidth - 40,
+                marginTop: 10,
+                borderWidth: 1,
+                borderColor: color.whiteBlue,
+                marginBottom: 5,
+                borderRadius: 4
+              },
+              textInputContainer: {
+                backgroundColor: color.whiteBlue,
+                padding: 0,
+                borderTopWidth: 0
+              },
+              textInput: {
+                width: '100%',
+                paddingLeft: 3,
+                paddingRight: 3,
+                fontSize: 18,
+                fontFamily: fontFamily.light,
+                backgroundColor: color.whiteBlue,
+                color: color.darkBlue
+              },
+
+              description: {
+                fontWeight: 'bold'
+              },
+              predefinedPlacesDescription: {
+                color: color.whiteBlue
               }
-            });
-          }}
-          getDefaultValue={() => ''}
-          query={{
-            // available options: https://developers.google.com/places/web-service/autocomplete
-            key: GOOGLE_API,
-            language: 'en' // language of the results
-            // types: '(cities)' // default: 'geocode'
-          }}
-          styles={{
-            container: {
-              width: '95%',
-              marginTop: 10,
-              borderWidth: 1,
-              borderColor: 'grey',
-              marginBottom: 5,
-              borderRadius: 4
-            },
-            textInputContainer: {
-              backgroundColor: 'rgba(0,0,0,0)',
-              padding: 0,
-              borderTopWidth: 0
-            },
-            textInput: {
-              width: '100%',
-              paddingLeft: 3,
-              paddingRight: 3
-            },
-
-            description: {
-              fontWeight: 'bold'
-            },
-            predefinedPlacesDescription: {
-              color: '#1faadb'
+            }}
+            // currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+            currentLocationLabel="Current location"
+            nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+            GoogleReverseGeocodingQuery={
+              {
+                // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+              }
             }
-          }}
-          // currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-          currentLocationLabel="Current location"
-          nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-          GoogleReverseGeocodingQuery={
-            {
-              // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-            }
-          }
-          GooglePlacesSearchQuery={{
-            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-            rankby: 'distance'
-            // types: 'food'
-          }}
-          filterReverseGeocodingByTypes={[
-            'locality',
-            'administrative_area_level_3'
-          ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-          debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-        />
+            GooglePlacesSearchQuery={{
+              // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+              rankby: 'distance'
+              // types: 'food'
+            }}
+            filterReverseGeocodingByTypes={[
+              'locality',
+              'administrative_area_level_3'
+            ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+            debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+          />
 
-        {/* below is the old location input */}
-        {/* <TextInput
-          style={styles.input}
-          mode="outlined"
-          label="Location"
-          value={this.state.location}
-          onChangeText={location => this.setState({ location })}
-        /> */}
-        <DatePicker
-          label="Date"
-          date={this.state.date}
-          mode="date"
-          showIcon={false}
-          style={styles.date}
-          onDateChange={date => this.setState({ date })}
-          placeholder="select date"
-          format="MM-DD-YYYY"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-        />
-        <DatePicker
-          mode="time"
-          showIcon={false}
-          date={this.state.time}
-          placeholder="select time"
-          style={styles.date}
-          onDateChange={time => this.setState({ time })}
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-        />
+          <DatePicker
+            label="Date"
+            date={this.state.date}
+            mode="date"
+            showIcon={false}
+            style={styles.picker}
+            onDateChange={date => this.setState({ date })}
+            placeholder="Select Date"
+            format="MM-DD-YYYY"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+          />
+          <DatePicker
+            mode="time"
+            showIcon={false}
+            date={this.state.time}
+            placeholder="Pick a Time"
+            style={styles.picker}
+            onDateChange={time => this.setState({ time })}
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+          />
+        </View>
+
         <View style={styles.bottom}>
           <Button
             onPress={this.handlePress}
-            title='next'
+            title="next"
             type="contained"
             disabled={
               !this.state.date ||
@@ -176,7 +177,7 @@ class CreateEvent extends Component {
               !this.state.location
             }
           >
-            <Text style={styles.butonText}>next</Text>
+            <Text style={styles.buttonText}>next</Text>
           </Button>
         </View>
       </View>
@@ -186,49 +187,57 @@ class CreateEvent extends Component {
 let styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start'
-    // backgroundColor: '#98B1C4'
+    backgroundColor: color.whiteBlue
   },
-  titleView: { padding: 10 },
-  input: {
-    // backgroundColor: '#C8D7E3',
-    borderColor: '#98B1C4',
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 5,
-    width: '95%'
+  titleView: {
+    padding: 10
+  },
+  inputContainer: {
+    width: windowWidth - 33,
+    borderRadius: 25,
+    paddingHorizontal: 16,
+    fontSize: 18,
+    fontFamily: fontFamily.light,
+    color: color.darkBlue,
+    marginVertical: 15,
+    alignSelf: 'center'
   },
   title: {
-    fontFamily: 'System',
-    fontSize: 40,
+    fontFamily: fontFamily.bold,
+    fontSize: fontSize.large,
     marginBottom: 0,
     paddingBottom: 0,
-    color: '#2F4E6F',
-    fontWeight: '500'
+    color: color.darkBlue,
+    alignSelf: 'center',
+    paddingTop: padding * 3
   },
-  date: {
+  picker: {
     marginBottom: 5,
-    // backgroundColor: '#C8D7E3',
     marginTop: 10,
-    width: '95%',
+    width: windowWidth - 40,
     justifyContent: 'center',
-    borderRadius: 5
+    borderRadius: 5,
   },
   bottom: {
     flex: 1,
     justifyContent: 'flex-end',
     marginBottom: 36
   },
-  butonText: {
+  buttonText: {
     fontSize: 30,
     position: 'absolute',
     bottom: 0
   },
   location: {
-    borderWidth: 0
+    borderWidth: 0,
+    alignSelf: 'center'
   },
-  button: {}
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: windowWidth - 40
+  }
 });
 
 const mapState = state => ({
